a8d31437fb3ac41d244f03e0a0f84808d99aead2
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-09 15:44:50 -0300

    chore: add products struct

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
 var templ = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templ.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{Name: "T-shirt", Description: "Blue, very beatiful", Price: 39, Quantity: 5},
+		{"Shoes", "Confortable", 89, 3},
+		{"Headphode", "Very good", 59, 2},
+		{"New Prodcut", "Very nice", 99, 1},
+	}
+	templ.ExecuteTemplate(w, "Index", products)
 }
