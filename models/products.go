798d2c2adf0f6b5aec8f1b5f6000d35dc28163d0
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-10 13:46:18 -0300

    chore: save into db

--- models/products.go
+++ models/products.go
@@ -41,3 +41,15 @@ func FindAllProducts() []Product {
 	defer db.Close()
 	return products
 }
+
+func CreateProduct(name, description string, price float64, quantity int) {
+	db := db.ConnectDatabase()
+
+	insertDbData, err := db.Prepare("INSERT INTO products(name, description, price, quantity) VALUES ($1, $2, $3, $4)")
+	if err != nil {
+		panic(err.Error())
+	}
+	insertDbData.Exec(name, description, price, quantity)
+
+	defer db.Close()
+}
